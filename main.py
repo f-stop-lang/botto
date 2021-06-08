e921d209ffbede167f9cb217c2785bd41265a2ec
MrKomodoDragon <74436682+MrKomodoDragon@users.noreply.github.com>
2021-06-07 18:10:42 -0700

    smh

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ class FStopBot(commands.Bot):
     
     def __init__(self) -> None:
         intents = discord.Intents.default()
-        intens.members = True
+        intents.members = True
+        self.color = 0x2F3136
         
         super().__init__(
             command_prefix=commands.when_mentioned_or('?', '!'),
             intents=intents,
             description='A bot for the f-stop Discord server.', 
             case_insensitive=True,
-            color=0x2F3136,
         )
         
  if __name__ == '__main__':
